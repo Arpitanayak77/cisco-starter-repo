f198797097a2437630b280e824e1d1cf4694209b
Arpitanayak77 <arpitaanayak1@gmail.com>
2023-06-29 21:50:08 +0530

    sextant: created banner

---
+++ sextant/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Banner bannerText="Sextant" />
+                <Exhibit name="I'm an exhibit too!"></Exhibit>
+                <Exhibit name="I'm also an exhibiting !"></Exhibit>
+                <Exhibit name="Don't forget about me, I'm an exhibit too!"></Exhibit>
+    </div>
+  );
+}
+
+export default App;
